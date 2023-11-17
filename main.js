65e8c5bad948bccdf88a99ff840806cd773505e5
Keymastertommy <thomashalsneshansen@gmail.com>
2023-11-17 10:49:11 +0100

    version 1

---
+++ main.js
@@ -0,0 +1,16 @@
+let names = ["Venom", "Carnage", "Scream", "Hybrid", "Spiderman "]
+
+
+let count = 0
+while(count < names.length){
+    count = count + 1
+    console.log(names[count])
+    
+}
+
+
+
+//function createList(name){}
+
+//let listElement = document.querySelector("names.list")
+
